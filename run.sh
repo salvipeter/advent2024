8a048d2f368d4e6f371829b7ec456a99fdaaa434
Peter Salvi <salvi@iit.bme.hu>
2024-12-05 11:45:36 +0100

    Formatting fix

--- run.sh
+++ run.sh
@@ -33,12 +33,12 @@ case $(printf "%02d" $1) in
 
     05)
         FILE=generated/adv05-gen.fs
-        awk 'BEGIN { FS = "[|,]"; print "CREATE rules" } \
-             NF == 2 { print $1 " , " $2 " ,"; next }                        \
-             NF > 0 { if (!rules) { print "0 ,"; print "CREATE pages" }      \
-                      rules = 1; p = $1;                                     \
-                      for (i = 2; i <= NF; i++) p = p " , " $i;              \
-                      print p " , 0 ," }                                     \
+        awk 'BEGIN { FS = "[|,]"; print "CREATE rules" }                \
+             NF == 2 { print $1 " , " $2 " ,"; next }                   \
+             NF > 0 { if (!rules) { print "0 ,"; print "CREATE pages" } \
+                      rules = 1; p = $1;                                \
+                      for (i = 2; i <= NF; i++) p = p " , " $i;         \
+                      print p " , 0 ," }                                \
              END { print "0 ," }' adv05.txt > $FILE
         cat adv05.fs >> $FILE
         gforth $FILE -e bye
