b56b6ce4ff6d707c18b148c25f534ea73a8cbb84
Peter Salvi <vukung@yahoo.com>
2024-12-06 13:06:00 +0100

    Some optimizations

--- adv06.hs
+++ adv06.hs
@@ -1,5 +1,6 @@
-import Data.List
-import Data.Maybe
+import Data.Array (listArray, (!))
+import Data.List (elemIndex, filter, findIndex, nub)
+import Data.Maybe (fromJust, maybe)
 
 data Dir = U | R | D | L deriving Eq
 
@@ -12,15 +13,23 @@ path pos dir | outside pos = []
              | blocked pos = path (step pos (back dir)) (turn dir)
              | otherwise   = pos : path (step pos dir) dir
 
+-- Optimization: remembers only the path _after_ the obtrusion
+-- (Interestingly, using a Set for acc would make it slower.)
 loops pos dir acc obt
-    | outside pos               = False
-    | elem (pos,dir) acc        = True
-    | pos == obt || blocked pos = loops (step pos (back dir)) (turn dir) (tail acc) obt
-    | otherwise                 = loops (step pos dir) dir ((pos,dir) : acc) obt
+    | outside next = False
+    | next == obt  = loops pos (turn dir) (maybe (Just []) Just acc) obt
+    | blocked next = loops pos (turn dir) acc obt
+    | visited      = True
+    | otherwise    = loops next dir (fmap ((next,dir) :) acc) obt
+    where next     = step pos dir
+          visited  = maybe False (elem (next,dir)) acc
 
 outside (x,y) = x < 0 || y < 0 || x >= width || y >= height
 
-blocked (x,y) = d !! y !! x == '#'
+-- Another optimization: blocked uses an array for O(1) indexing
+mapArray = listArray ((0,0),(height-1,width-1)) $ concat d
+
+blocked (x,y) = mapArray ! (y,x) == '#'
 
 step (x,y) U = (x,y-1)
 step (x,y) R = (x+1,y)
@@ -36,5 +45,5 @@ turn L = U
 
 main = putStrLn (show $ length visited) >> putStrLn (show obtrusions)
     where visited     = nub $ path (x,y) U
-          obtrusions  = length $ filter (loops (x,y) U []) candidates
+          obtrusions  = length $ filter (loops (x,y) U Nothing) candidates
           candidates  = [pos | pos <- visited, pos /= (x,y)]
