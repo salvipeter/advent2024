a50e8b19246041d295c46b613fc38b1457d70017
Peter Salvi <vukung@yahoo.com>
2024-12-11 07:12:41 +0100

    Deleted some unnecessary cuts

--- adv11.pl
+++ adv11.pl
@@ -5,8 +5,8 @@ solve :-
 all_stones(N, L, X) :- maplist(stone(N), L, Xs), sumlist(Xs, X).
 
 :- table(stone/3).
-stone(0, _, 1) :- !.
-stone(K, 0, X) :- !, K > 0, K1 is K - 1, stone(K1, 1, X).
+stone(0, _, 1).
+stone(K, 0, X) :- K > 0, !, K1 is K - 1, stone(K1, 1, X).
 stone(K, N, X) :-
     K > 0, K1 is K - 1,
     halves(N, L, R), !, stone(K1, L, X1), stone(K1, R, X2),
@@ -17,15 +17,15 @@ halves(N, L, R) :-
     digits(N, D), length(D, ND), ND mod 2 =:= 0, K is ND // 2,
     take(D, K, Ls, Rs), build(Ls, L), build(Rs, R).
 
-take(Xs, 0, [], Xs) :- !.
+take(Xs, 0, [], Xs).
 take([X|Xs], K, [X|L], R) :- K > 0, K1 is K - 1, take(Xs, K1, L, R).
 
 % Both build/2 and digits/2 are simpler when digits are reversed
 
-build([X], X) :- !.
+build([X], X).
 build([X|Xs], N) :- build(Xs, N0), N is N0 * 10 + X.
 
-digits(N, [N]) :- N < 10, !.
+digits(N, [N]) :- N < 10.
 digits(N, [D|L]) :-
     N >= 10,
     D is N mod 10, N1 is N // 10,
